3dc7a808266036651beb7b3e7a209be97515e97c
Benjamin Grbo <benjamin.grbo@fsk.unsa.ba>
2022-05-28 17:25:48 +0200

    zavrsene osiguravajuce kuce

---
+++ bbINFO/bbINFO/Osiguranja2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace bbINFO
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Osiguranja2 : ContentPage
+    {
+        public Osiguranja2(Grad proslijedjeniGrad)
+        {
+            InitializeComponent();
+
+
+            osiguravajuceKuceView.ItemsSource = proslijedjeniGrad.ListaOsiguravajucihKuca;
+        }
+
+        private async void osiguravajuceKuceView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            OsiguravajuceKuce oznacenoOsiguranje = (OsiguravajuceKuce) e.SelectedItem;
+
+            await Navigation.PushAsync(new Osiguranja3(oznacenoOsiguranje));
+
+        }
+    }
+}
